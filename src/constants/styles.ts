80e53e876a6cfc6ceefbcb37720cdf9ada49aa7b
kolynzb <collinsbenda360@gmail.com>
2022-06-22 07:55:06 +0300

    Add Reapeated Styles

---
+++ src/constants/styles.ts
@@ -0,0 +1,20 @@
+export default {
+  horrizontalPadding: 25,
+  verticalPadding: 20,
+  h1: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  h2: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  p: {
+    color: "grey",
+    fontSize: 12,
+  },
+  pWhiteStyle: {
+    color: "white",
+    fontSize: 12,
+  },
+};
