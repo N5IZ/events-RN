f9242638b84e713eec29f313f16f568b34326e41
kolynzb <collinsbenda360@gmail.com>
2022-06-22 16:58:36 +0300

    Add tailwind

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,12 +1,14 @@
 import { View, Text } from "../components/Themed";
 import React from "react";
+import { useTailwind } from "tailwind-rn";
 
 type Props = {};
 
 const SearchScreen = (props: Props) => {
+  const tailwind = useTailwind();
   return (
     <View>
-      <Text>SearchScreen</Text>
+      <Text style={tailwind("font-bold")}>SearchScreen</Text>
     </View>
   );
 };
