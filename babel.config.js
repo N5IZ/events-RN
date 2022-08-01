2346bcdf224721f6869c732771baefd11b6b198e
kolynzb <collinsbenda360@gmail.com>
2022-08-01 12:12:37 +0300

    Convert expo to RN CLI

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,3 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo", "react-native-reanimated/plugin"],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
 };
